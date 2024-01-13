265c67b9ae44a5cc8afea22195c441f2b919e473
AryanAAB <145079379+AryanAAB@users.noreply.github.com>
2024-01-13 08:40:53 +0530

    Add files via upload

---
+++ Utilities.py
@@ -0,0 +1,24 @@
+import sys
+
+def checkType(checkList:list):
+    """
+    checkList must be a list of the form
+    [(arg1, type1), (arg2, type2), (arg3, type3), ..., (argN, typeN)]
+    Returns true if all the types specified are correct and none of the
+    arguments are None.
+    Throws an Error and exits otherwise.
+    """
+
+    for arg in checkList:
+        if(arg[0] == None or not isinstance(arg[0], arg[1])):
+            printErrorAndExit(f"{arg[0]} is not of valid type {arg[1]}.")
+
+    return True
+
+def printErrorAndExit(message:str):
+        """
+        This function prints the error message specified by message and exits. 
+        """
+
+        print(message)
+        sys.exit(1)
