597c4a5b7f36d346e3bd42aaa63d909f8cc43eb2
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-01-16 12:55:43 +0530

    Update Register.py

--- Register.py
+++ Register.py
@@ -7,13 +7,13 @@ class Register:
         
         ans=bin(self.__val)[2:]
         
-        '''while (len(ans)>self.__size):
+        while (len(ans)>self.__size):
             self.__val-=2**self.__size
             print("overflow")
             ans=bin(self.__val)[2:]
             
         while (self.__size-len(ans)):
-            ans='0'+ans'''
+            ans='0'+ans
             
         return ans
 
@@ -42,15 +42,15 @@ class Register:
         
         self.__val=int((curr[:start]+req+curr[end:]),2)
 
-
-IR=Register(8,3)
-IBR=Register(20,837762)
-MAR=Register(12)
-print(IR,IBR,MAR,end='\n\n',sep='\n')
-print(Register.read(IR))
-print(IBR.read())
-print(IR.read())
-IBR.write(15,0,4)
-print(IBR.read())
+if __name__ == __main__:
+    IR=Register(8,3)
+    IBR=Register(20,837762)
+    MAR=Register(12)
+    print(IR,IBR,MAR,end='\n\n',sep='\n')
+    print(Register.read(IR))
+    print(IBR.read())
+    print(IR.read())
+    IBR.write(15,0,4)
+    print(IBR.read())
         
         
