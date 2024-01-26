62ddb2160b02718078703c61807412ea7cdbd7d0
AryanAAB <145079379+AryanAAB@users.noreply.github.com>
2024-01-26 23:21:21 +0530

    Add files via upload

--- IMT2023029_main.py
+++ IMT2023029_main.py
@@ -3,7 +3,7 @@ from IMT2023029_Processor import Processor
 
 asmFile = "IMT2023029_Assembly.asm"
 executableFile = "IMT2023029_Assembly.exe"
-outputFile = "IMT2023029_Output.txt"
+outputFile = "Output.txt"
 
 Assembler(asmFile).run()
 Processor(executableFile, outputFile).run(20, 999)
