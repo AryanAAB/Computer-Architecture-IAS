240757aa19ada69e45363d4f923a95c0f0b0c020
AryanAAB <145079379+AryanAAB@users.noreply.github.com>
2024-01-26 22:25:45 +0530

    Update and rename ExecuteWindows.bat to IMT2023513_ExecuteWindows.bat

--- ExecuteWindows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem Run Python program
-python3 main.py
-
-rem Run Java program
-javac GUI.java
-java GUI
---
+++ IMT2023513_ExecuteWindows.bat
@@ -0,0 +1,7 @@
+@echo off
+rem Run Python program
+python3 IMT2023513_main.py
+
+rem Run Java program
+javac IMT2023513_GUI.java
+java IMT2023513_GUI
