bf2284c7730b59fa520ba1ea3e1142768e63bdfa
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-01-26 22:49:28 +0530

    Update IMT2023029_main.py

--- IMT2023029_main.py
+++ IMT2023029_main.py
@@ -1,5 +1,5 @@
-from Assembler import Assembler
-from Processor import Processor
+from IMT2023029_Assembler import Assembler
+from IMT2023029_Processor import Processor
 
 asmFile = "IMT2023029_Assembly.asm"
 executableFile = "IMT2023029_Assembly.exe"
