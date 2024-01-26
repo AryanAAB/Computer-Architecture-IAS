417896fb25467ca1adea8d374f4e46f41c61d1d1
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-01-26 17:03:45 +0530

    Update Assembly.asm

--- Assembly.asm
+++ Assembly.asm
@@ -2,6 +2,18 @@ _include "InsertionSort.asm"
 _include "BinSearch.asm"
 
 _start
+
+    //Getting the input from the user
+    20 INP M(80); LOAD -M(80);                    //Gets user input for array length
+    21 STOR M(81); JUMP+ M(26,20:39);             //Stores the array length in temporary memory 81
+    22 LOAD -M(81); ADD M(499); 
+    23 STOR M(24,8:19); NOP;
+    24 INP M(0); LOAD M(81);                      //Gets the user value and puts into the memory
+    25 ADD M(503); STOR M(81);                    //Decreases temp memory by one and stores it there
+    26 JUMP M(21,20:39); NOP;                     //Goes back to first instruction
+
+    //Pre-Defined values and important locations
+
     80 0;       //Array length to be filled by user
     81 0;       //Temporary memory location used by the program
     82 1;       //Stores the value i
