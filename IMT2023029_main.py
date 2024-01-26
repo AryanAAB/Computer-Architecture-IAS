aa77706f7072bc1bf3886570db1550f00d27d556
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-01-26 22:36:34 +0530

    Update and rename main.py to IMT2023029_main.py

--- IMT2023029_main.py
+++ IMT2023029_main.py
@@ -1,9 +1,9 @@
 from Assembler import Assembler
 from Processor import Processor
 
-asmFile = "Assembly.asm"
-executableFile = "Assembly.exe"
-outputFile = "Output.txt"
+asmFile = "IMT2023029_Assembly.asm"
+executableFile = "IMT2023029_Assembly.exe"
+outputFile = "IMT2023029_Output.txt"
 
 Assembler(asmFile).run()
 Processor(executableFile, outputFile).run(20, 999)
