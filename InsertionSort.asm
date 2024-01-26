e9fb83defc62b70bfc56e781af0d00bb99c354db
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-01-26 17:04:43 +0530

    Update InsertionSort.asm

--- InsertionSort.asm
+++ InsertionSort.asm
@@ -7,15 +7,6 @@ By Aryan Bansal and Abhirath Adamane
 
 _start
 
-    //Getting the input from the user
-    20 INP M(80); LOAD -M(80);                    //Gets user input for array length
-    21 STOR M(81); JUMP+ M(26,20:39);             //Stores the array length in temporary memory 81
-    22 LOAD -M(81); ADD M(499); 
-    23 STOR M(24,8:19); NOP;
-    24 INP M(0); LOAD M(81);                      //Gets the user value and puts into the memory
-    25 ADD M(503); STOR M(81);                    //Decreases temp memory by one and stores it there
-    26 JUMP M(21,20:39); NOP;                     //Goes back to first instruction
-
     //Sorts the array
     27 LOAD M(82); SUB M(80);                     //For loop condition
     28 JUMP+ M(47,0:19); LOAD M(82);              //If condition is false, exit
